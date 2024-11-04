0153017eba4b2a6e59b514234f26c94395cf1a91
Anton <antonsamoel14@gmail.com>
2024-11-04 21:34:16 +0200

    Finish First Microservice (Catalog.api) with containerization

---
+++ Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿
+using FluentValidation;
+
+namespace Catalog.Api.Products.CreateProduct
+{
+    #region Request
+
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+  : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    #endregion
+
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+        }
+
+    }
+
+    public class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+             // Create the new product
+            var product = new Product
+            {
+                Name = request.Name,
+                Category = request.Category,
+                Description = request.Description,
+                ImageFile = request.ImageFile,
+                Price = request.Price
+            };
+
+            // Add product to database
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            // Return the response
+            return new CreateProductResult(product.Id);
+        }
+    }
+}
